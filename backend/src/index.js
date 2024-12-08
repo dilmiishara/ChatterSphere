864e0d19d156f3fe22d2b93c29b1dc91bd383540
dilmiishara2002 <dilmiishara2000@gmail.com>
2024-12-08 17:13:18 +0530

    Implement authentication features: login, logout, update profile picture, and check auth functions in auth.controller.js. Add protectRoute middleware for route protection in auth.middleware.js.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,8 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config()
@@ -10,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 
